d80afd76f1b622adba66d54e87744341613d5472
Yelena <yelenabel@gmail.com>
2015-08-26 10:19:47 -0700

    create method all to return array of objects in list

--- src/main/java/Restaurant.java
+++ src/main/java/Restaurant.java
@@ -23,4 +23,21 @@ public class Restaurant {
   public int getCuisine_id() {
     return cuisine_id;
   }
+
+  //constructor method, initializes an instance of Restaurant
+  public Restaurant (String name, String hours, String contact_info, int rating, int cuisine_id) {
+    this.name = name;
+    this.hours = hours;
+    this.contact_info = contact_info;
+    this.rating = rating;
+    this.cuisine_id = cuisine_id;
+  }
+
+  public static List<Restaurant> all()  {
+    String sql = "SELECT name, hours, contact_info, rating, cuisine_id FROM restaurants";
+    try(Connection con = DB.sql2o.open()) {
+      return con.createQuery(sql).executeAndFetch(Restaurant.class);
+    }
+  }
+
 }
