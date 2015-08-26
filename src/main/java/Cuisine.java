27bb3f9951da6b7871c6dd72416b309a6b55303e
Yelena <yelenabel@gmail.com>
2015-08-26 12:01:20 -0700

    add a find method to return an object from the db by id

--- src/main/java/Cuisine.java
+++ src/main/java/Cuisine.java
@@ -5,19 +5,48 @@ public class Cuisine {
   private int id;
   private String name;
 
-  public String getName {
+  public String getName() {
     return name;
   }
-  public int getInt() {
+  public int getId() {
     return id;
   }
   public Cuisine(String name) {
     this.name = name;
   }
   public static List<Cuisine> all() {
-    String sql = "SELECT name FROM restaurants";
+    String sql = "SELECT name FROM cuisines";
     try(Connection con = DB.sql2o.open()) {
       return con.createQuery(sql).executeAndFetch(Cuisine.class);
+    }
+  }
+
+  @Override
+  public boolean equals(Object otherCuisine) {
+    if(!(otherCuisine instanceof Cuisine)) {
+      return false;
+    } else {
+      Cuisine newCuisine = (Cuisine) otherCuisine;
+      return this.getName().equals(newCuisine.getName());
+    }
+  }
+
+  public void save() {
+    try(Connection con = DB.sql2o.open()) {
+      String sql = "INSERT INTO cuisines (name) VALUES (:name)";
+      this.id = (int) con.createQuery(sql, true)
+        .addParameter("name", this.name)
+        .executeUpdate()
+        .getKey();
+    }
+  }
+  public static Cuisine find(int id) {
+    try(Connection con = DB.sql2o.open()) {
+      String sql = "SELECT * FROM cuisines WHERE id=:id";
+      Cuisine cuisine = con.createQuery(sql)
+        .addParameter("id", id)
+        .executeAndFetchFirst(Cuisine.class);
+        return cuisine;
     }
   }
 }
