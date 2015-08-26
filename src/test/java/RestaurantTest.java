1d79c7ca777c92f978e17fd4e278128a8e9c8212
Yelena <yelenabel@gmail.com>
2015-08-26 10:52:04 -0700

    create test to make sure fund method returns the correct object from the database

--- src/test/java/RestaurantTest.java
+++ src/test/java/RestaurantTest.java
@@ -14,8 +14,25 @@ import java.util.Arrays;
     @Test
     public void equals_returnTrueIfRestaurantsAreTheSame() {
       Restaurant firstRestaurant = new Restaurant("mi mero mole", "9:00-15-00", "23424231", 5, 1);
-
+      Restaurant secondRestaurant = new Restaurant("mi mero mole", "9:00-15-00", "23424231", 5, 1);
+      assertTrue(firstRestaurant.equals(secondRestaurant));
     }
+
+    @Test
+    public void save_savesIntoDatabase_true() {
+      Restaurant myRestaurant = new Restaurant("mi mero mole", "9:00-15-00", "23424231", 5, 1);
+      myRestaurant.save();
+      assertTrue(Restaurant.all().get(0).equals(myRestaurant));
+    }
+
+    @Test
+    public void find_findRestaurantInDatabase_true() {
+      Restaurant myRestaurant = new Restaurant("mi mero mole", "9:00-15-00", "23424231", 5, 1);
+      myRestaurant.save();
+      Restaurant savedRestaurant = Restaurant.find(myRestaurant.getId());
+      assertTrue(myRestaurant.equals(savedRestaurant));
+    }
+
     //
     // private String name;
     // private String hours;
