82577ba879e1418a2754847bbc89527693004787
Yelena Belikova <yelenabel@gmail.com>
2015-08-26 22:01:25 -0700

    change INSERT statement to include if, was not saving id in database

--- src/main/java/Cuisine.java
+++ src/main/java/Cuisine.java
@@ -15,7 +15,7 @@ public class Cuisine {
     this.name = name;
   }
   public static List<Cuisine> all() {
-    String sql = "SELECT name FROM cuisines";
+    String sql = "SELECT name, id FROM cuisines";
     try(Connection con = DB.sql2o.open()) {
       return con.createQuery(sql).executeAndFetch(Cuisine.class);
     }
