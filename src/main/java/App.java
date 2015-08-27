304d6f59a7507a46b76a5e7ddd849d4c41eeb762
Yelena <yelenabel@gmail.com>
2015-08-27 12:11:32 -0700

    make it so it returns a list of Restaurant based on matchung cuisine_id to thr url cuisine id

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -71,7 +71,7 @@ public class App{
 
       Restaurant newRestaurant = new Restaurant(name, hours, contact_info, rating, cuisine_id);
       newRestaurant.save();
-      
+
       model.put("cuisine", Cuisine.find(Integer.parseInt(request.queryParams("cuisine_id"))));
       model.put("restaurant", newRestaurant);
       return new ModelAndView(model, layout);
@@ -82,7 +82,7 @@ public class App{
       model.put("template", "templates/restaurants.vtl");
 
       model.put("cuisine", Cuisine.find(Integer.parseInt(request.params(":id"))));
-      model.put("restaurants", Restaurant.all());
+      model.put("restaurants", Restaurant.allByCuisine(Integer.parseInt(request.params(":id"))));
       return new ModelAndView(model, layout);
     }, new VelocityTemplateEngine());
 
@@ -94,5 +94,39 @@ public class App{
       model.put("restaurant", Restaurant.find(Integer.parseInt(request.params(":restaurant_id"))));
       return new ModelAndView(model, layout);
     }, new VelocityTemplateEngine());
+
+    get("/cuisines/:cuisine_id/restaurants/:restaurant_id/update", (request, response) -> {
+      HashMap<String, Object> model = new HashMap<String, Object>();
+      model.put("template", "templates/update.vtl");
+
+      model.put("cuisine", Cuisine.find(Integer.parseInt(request.params(":cuisine_id"))));
+      model.put("restaurant", Restaurant.find(Integer.parseInt(request.params(":restaurant_id"))));
+      return new ModelAndView(model, layout);
+    }, new VelocityTemplateEngine());
+
+    post("/info", (request, response) -> {
+      HashMap<String, Object> model = new HashMap<String, Object>();
+
+      String name = request.queryParams("name");
+      String hours = request.queryParams("hours");
+      String contact_info = request.queryParams("contact_info");
+      int rating = Integer.parseInt(request.queryParams("rating"));
+
+      Restaurant rest = Restaurant.find(Integer.parseInt(request.queryParams("restaurant_id")));
+
+      rest.updateName(name);
+      rest.updateHours(hours);
+      rest.updateContact_info(contact_info);
+      rest.updateRating(rating);
+
+      model.put("restaurant", Restaurant.find(Integer.parseInt(request.queryParams("restaurant_id"))));
+      model.put("template", "templates/success.vtl");
+      return new ModelAndView(model, layout);
+    }, new VelocityTemplateEngine());
+
+
   }
+
+
+
 }
